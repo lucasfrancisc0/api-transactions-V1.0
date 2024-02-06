85cc990aced253ff91183c712dae2daf59f1e5dc
Lucas <lucasarnoldd@gmail.com>
2024-02-06 11:03:21 -0300

    Install knex and sqlite3. Create config knex

---
+++ src/knex/index.ts
@@ -0,0 +1,20 @@
+import { Knex, knex as setupKnex } from "knex";
+import { env } from "../env"
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+
+  useNullAsDefault: true,
+
+  migrations: {
+    extension: 'ts',
+    directory: './src/knex/migrations',
+  },
+
+}
+
+export const knex = setupKnex(config);
